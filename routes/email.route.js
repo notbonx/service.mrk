1f880d0b2cc6ba442448989e3b144386611a6028
антоша))) <68808678+notbonx@users.noreply.github.com>
2022-06-30 21:50:54 +0300

    Update email.route.js

--- routes/email.route.js
+++ routes/email.route.js
@@ -5,6 +5,7 @@ router.post('/about', function (req, res) {
     res.header('Access-Control-Allow-Origin', '*')
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header('Vary', 'Origin')
     res.send('About this wiki');
     res.end()
 })
