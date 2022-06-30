5cfdddce102e4ea6c8abe4a2cd4b8bcf824b4bd9
антоша))) <68808678+notbonx@users.noreply.github.com>
2022-06-30 21:46:17 +0300

    Update email.route.js

--- routes/email.route.js
+++ routes/email.route.js
@@ -2,7 +2,12 @@ const express = require("express")
 const router = express.Router();
 
 router.post('/about', function (req, res) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     res.send('About this wiki');
+    res.end()
 })
+
 
 module.exports = router
