8bbf922c23129138b8669ba58298d4690a6bff05
антоша))) <68808678+notbonx@users.noreply.github.com>
2022-06-30 21:38:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000
 
 app.use(cors({
     origin: 'https://service.mrk-bsuir.by/',
-    optionsSuccessStatus: 200
+    optionsSuccessStatus: 200}
     ))
 app.use(bodyParser.json())
 
